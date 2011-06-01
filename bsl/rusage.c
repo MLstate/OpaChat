89391bea48e5ec632e01497dd0188f00e93a7fad
Frederic Ye <frederic.ye@mlstate.com>
2011-06-01 17:48:04 +0200

    [binding] reduced includes in c file

--- bsl/rusage.c
+++ bsl/rusage.c
@@ -1,8 +1,4 @@
-#include <stdlib.h>
 #include <sys/resource.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 long usage() {
   int who = RUSAGE_SELF;
@@ -12,12 +8,7 @@ long usage() {
   return p->ru_maxrss;
 }
 
-#include <errno.h>
-#include <caml/mlvalues.h>
 #include <caml/memory.h>
-#include <caml/alloc.h>
-#include <caml/compatibility.h>
-#include <caml/fail.h>
 
 value get_memory_usage() {
   CAMLparam0();
